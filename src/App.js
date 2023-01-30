48db13887f48e89895138ddcd09ad1e06410e5b2
Zack Jones <zajonesck83@gmail.com>
2023-01-29 19:12:47 -0600

    imported react and useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
-
-function calcAge(age) {
-  return age * 365;
-}
-console.log(calcAge(10));
+import React, { useState } from "react";
 
 function App() {
+  const [ageInYears, setAgeInYears] = useState(10);
+  const [ageInDays, setAgeInDays] = useState();
+
+  const handleChange = (e) => setAgeInYears(e.target.ageInYears);
+  function calcAge() {
+    const days = ageInYears * 365 + (ageInYears % 4);
+    setAgeInDays(days);
+    console.log(days);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Age to days.</p>
+        <h1>Age to days</h1>
+        <input
+          value={ageInYears}
+          onChange={handleChange}
+          type="number"
+          id="yourAge"
+          placeholder="age in years"
+        ></input>
+
+        <button onClick={calcAge}>Days</button>
+
+        <p>{ageInDays}</p>
+
         <a
           className="App-link"
           href="https://github.com/zajonesck"
@@ -22,5 +40,17 @@ function App() {
     </div>
   );
 }
+// function Square({ val, dataTestId, chooseSquare }) {
+//   return (
+//     <div
+//       className="square"
+//       data-testid={dataTestId}
+//       id={dataTestId}
+//       onClick={chooseSquare}
+//     >
+//       {val}
+//     </div>
+//   );
+// }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import "./App.css";
 
-test('renders learn react link', () => {
+test("renders GitHub link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Check out my GitHub here/i);
   expect(linkElement).toBeInTheDocument();
 });
