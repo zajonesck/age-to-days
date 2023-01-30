038f61ac46659a7dcc971508bbde35758027891a
Zack Jones <zajonesck83@gmail.com>
2023-01-30 15:29:05 -0600

    date picker shows date but not used in logic yet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import React, { useState } from "react";
+import DatePicker from "react-date-picker";
 
 function App() {
   const [ageInYears, setAgeInYears] = useState("");
   const [ageInDays, setAgeInDays] = useState();
+
+  const [value, onChange] = useState(new Date());
 
   function handleChange(e) {
     setAgeInYears(e.target.value);
@@ -14,22 +17,36 @@ function App() {
     const days = ageInYears * 365 + Math.floor(ageInYears / 4);
     setAgeInDays(days);
   }
+  // To set two dates to two variables
+  const date1 = new Date();
+
+  //make date2 equal the input from date picker
+  let date2 = new Date("07/30/2019");
+  console.log("date1", date1);
+
+  console.log("date2", date2);
+
+  let differenceInTime = date1.getTime() - date2.getTime();
+
+  let differenceInDays = differenceInTime / (1000 * 3600 * 24);
+  console.log(
+    "Total number of days between dates " +
+      date1 +
+      date2 +
+      " is: " +
+      differenceInDays
+  );
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Age to days</h1>
-        <input
-          value={ageInYears}
-          onChange={handleChange}
-          type="number"
-          id="yourAge"
-          placeholder="age in years"
-        ></input>
+        <h3>Enter your birthday</h3>
+        <DatePicker onChange={onChange} value={value} />
 
         <button onClick={calcAge}>Days</button>
 
-        <p>You're {ageInDays} days old.</p>
+        <p>You're {ageInDays} days old</p>
 
         <a
           className="App-link"
